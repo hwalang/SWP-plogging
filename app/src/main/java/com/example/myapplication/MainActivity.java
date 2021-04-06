ff48dbb689397c3321fe68896b9d39747b668d78
hwalang <smdong97@gmail.com>
2021-04-06 23:18:51 +0900

    multidex 에러 해결
    김동현

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* 인증 버튼 클릭시 BottomNavigation 으로 전환
+        /* 로그인이후 버튼 클릭시 BottomNavigation 으로 전환
         * */
         main_button = findViewById(R.id.main_button);
         main_button.setOnClickListener(view -> {
