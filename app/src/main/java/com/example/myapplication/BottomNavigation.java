0b09b5a2cb1f81a705ce28d8d37a6bbd77204fd7
wjdrlwn0117 <77970721+wjdrlwn0117@users.noreply.github.com>
2021-05-03 20:37:15 +0900

    바텀 내비게이션 유저부분 제거

--- app/src/main/java/com/example/myapplication/BottomNavigation.java
+++ app/src/main/java/com/example/myapplication/BottomNavigation.java
@@ -24,7 +24,7 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class BottomNavigation extends AppCompatActivity {
-    NavigationUserFragment navigationUserFragment;
+
     NavigationPloggingFragment navigationPloggingFragment;
     NavigationCertifyFragment navigationCertifyFragment;
     NavigationRecruitFragment navigationRecruitFragment;
@@ -36,14 +36,14 @@ public class BottomNavigation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bottom_navigation);
 
-        navigationUserFragment = new NavigationUserFragment();
+
         navigationPloggingFragment = new NavigationPloggingFragment();
         navigationCertifyFragment = new NavigationCertifyFragment();
         navigationRecruitFragment = new NavigationRecruitFragment();
         navigationIssueFragment = new NavigationIssueFragment();
 
         // 첫 화면
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, navigationUserFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_content, navigationPloggingFragment).commit();
 
         // 내비게이션 이벤트
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -61,11 +61,6 @@ public class BottomNavigation extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 item -> {
                     switch (item.getItemId()) {
-                        case R.id.action_user:
-                            Toast.makeText(getApplicationContext(), "user 선택", Toast.LENGTH_SHORT).show();
-                            // main_content: fragment id
-                            getSupportFragmentManager().beginTransaction().replace(R.id.main_content, navigationUserFragment).commit();
-                            return true;
                         case R.id.action_plogging:
                             Toast.makeText(getApplicationContext(), "plogging 선택", Toast.LENGTH_SHORT).show();
                             getSupportFragmentManager().beginTransaction().replace(R.id.main_content, navigationPloggingFragment).commit();
