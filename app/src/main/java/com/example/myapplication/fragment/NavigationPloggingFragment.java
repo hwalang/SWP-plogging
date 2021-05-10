1c4349921d272c4a5006e55999b6046ced204fa3
Leechaeyeon <52447756+dkin12@users.noreply.github.com>
2021-05-10 18:24:22 +0900

    buttonclick이벤트
    이벤트와 디자인 완료

--- app/src/main/java/com/example/myapplication/fragment/NavigationPloggingFragment.java
+++ app/src/main/java/com/example/myapplication/fragment/NavigationPloggingFragment.java
@@ -1,21 +1,54 @@
 package com.example.myapplication.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.Mountine;
+import com.example.myapplication.Park;
 import com.example.myapplication.R;
+import com.example.myapplication.Walk;
 
 public class NavigationPloggingFragment extends Fragment {
+    Button walk,park,moun;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_bottom_main_plogging, container, false);
+        View view = inflater.inflate(R.layout.fragment_bottom_main_plogging, container, false);
+
+        walk = view.findViewById(R.id.walk);
+        walk.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getActivity(),Walk.class);
+                startActivity(intent);
+            }
+        });
+        park = view.findViewById(R.id.park);
+        park.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getActivity(),Park.class);
+                startActivity(intent);
+            }
+        });
+        moun = view.findViewById(R.id.mountine);
+        moun.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getActivity(),Mountine.class);
+                startActivity(intent);
+            }
+        });
+
+    return view;
     }
 
 }
+
+
